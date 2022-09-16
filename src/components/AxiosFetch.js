3b732a8541bfc2fe90d90eff11478e92a2ef880e
MatiasDBrites <matias.brites@gmail.com>
2022-09-16 12:53:48 -0500

    Se agrego el archivo de babel para arreglar el problema

--- src/components/AxiosFetch.js
+++ src/components/AxiosFetch.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API = "https://hn.algolia.com/api/v1"
+export const API = "https://hn.algolia.com/api/v3"
 
 export const fetchData = async query => {
   const url = `${API}/search?query=${query}`;
